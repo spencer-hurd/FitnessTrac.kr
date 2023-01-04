03a995d747a4ccb4adfdb12b28637af8388d717b
spencer-hurd <schurd02@gmail.com>
2023-01-04 13:31:17 -0700

    api routers started

--- api/index.js
+++ api/index.js
@@ -1,24 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
 
 // GET /api/health
-router.get('/health', async (req, res, next) => {
+router.get("/health", async (req, res, next) => {
+  res.send({
+    message: "Server Working",
+  });
 });
 
 // ROUTER: /api/users
-const usersRouter = require('./users');
-router.use('/users', usersRouter);
+const usersRouter = require("./users");
+router.use("/users", usersRouter);
 
 // ROUTER: /api/activities
-const activitiesRouter = require('./activities');
-router.use('/activities', activitiesRouter);
+const activitiesRouter = require("./activities");
+router.use("/activities", activitiesRouter);
 
 // ROUTER: /api/routines
-const routinesRouter = require('./routines');
-router.use('/routines', routinesRouter);
+const routinesRouter = require("./routines");
+router.use("/routines", routinesRouter);
 
 // ROUTER: /api/routine_activities
-const routineActivitiesRouter = require('./routineActivities');
-router.use('/routine_activities', routineActivitiesRouter);
+const routineActivitiesRouter = require("./routineActivities");
+router.use("/routine_activities", routineActivitiesRouter);
+
+router.use((error, req, res, next) => {
+  res.send({
+    name: error.name,
+    message: error.message,
+  });
+});
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
-
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const app = express();
+const apiRouter = require("./api");
+app.use(morgan("dev"));
+app.use(cors());
 // Setup your Middleware and API Router here
+app.use("/api", apiRouter);
 
 module.exports = app;
