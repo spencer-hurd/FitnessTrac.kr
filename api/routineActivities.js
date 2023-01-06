ea8ccc6b0ec5326a8840f5a60b8f8103383acf96
spencer-hurd <schurd02@gmail.com>
2023-01-06 12:27:33 -0700

    final commit

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -3,8 +3,14 @@ const {
   getRoutineActivityById,
   getRoutineById,
   updateRoutineActivity,
+  destroyRoutineActivity,
 } = require("../db");
-const { UnauthorizedError, UnauthorizedUpdateError } = require("../errors");
+const {
+  UnauthorizedError,
+  UnauthorizedUpdateError,
+  UnauthorizedDeleteError,
+} = require("../errors");
+const validateToken = require("./helpers");
 const router = express.Router();
 
 // PATCH /api/routine_activities/:routineActivityId
@@ -52,5 +58,44 @@ router.patch("/:routineActivityId", async (req, res, next) => {
   }
 });
 // DELETE /api/routine_activities/:routineActivityId
+router.delete("/:routineActivityId", async (req, res, next) => {
+  const routineActivityId = req.params.routineActivityId;
+  try {
+    const isValidToken = await validateToken(req);
+
+    if (!isValidToken) {
+      res.status(401);
+      next({
+        error: "UnauthorizedError",
+        name: "UnauthorizedError",
+        message: UnauthorizedError(),
+      });
+    }
+    const _routineActivity = await getRoutineActivityById(routineActivityId);
+    if (!_routineActivity) {
+      next({
+        error: "RoutineActivityNotFoundError",
+        name: "RoutineActivityNotFoundError",
+        message: `Routine Activity ${routineActivityId} not found.`,
+      });
+    }
+    const _routine = await getRoutineById(_routineActivity.routineId);
+    if (_routine.creatorId !== isValidToken.id) {
+      res.status(403);
+      next({
+        error: "UnauthorizedDeleteError",
+        name: "UnauthorizedDeleteError",
+        message: UnauthorizedDeleteError(isValidToken.username, _routine.name),
+      });
+    }
+    const destroyedRoutineActivity = await destroyRoutineActivity(
+      routineActivityId
+    );
+    destroyedRoutineActivity.success = true;
+    res.send(destroyedRoutineActivity);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
--- app.js
+++ app.js
@@ -10,8 +10,27 @@ app.use(express.json());
 // Setup your Middleware and API Router here
 app.use("/api", apiRouter);
 
+app.get("*", (req, res) => {
+  res.status(404).send({
+    error: "404 - Not Found",
+    message: "No route found for the requested URL",
+  });
+});
+
 app.use((error, req, res, next) => {
-  res.send(error);
+  console.error("SERVER ERROR: ", error);
+  console.log({
+    error: error.message,
+    name: error.name,
+    message: error.message,
+    table: error.table,
+  });
+  res.send({
+    error: error.message,
+    name: error.name,
+    message: error.message,
+    table: error.table,
+  });
 });
 
 module.exports = app;
